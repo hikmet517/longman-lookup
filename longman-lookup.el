3705ba340af5831c45ff6e297af7cfd2bde5114a
Hikmet Altıntaş <hikmet1517@gmail.com>
2020-11-20 18:58:41 +0300

    handle word references refactoring etc.
    handle 'Crossref' class
    make resulting buffer read-only
    use same buffer for repeated queries
    change buffer name (*ldoce <word>*)
    parse-entry function now extracts entry name (no need to pass it as a parameter)
    don't write noun, verb info if there is none

--- longman-lookup.el
+++ longman-lookup.el
@@ -10,14 +10,21 @@
 ;; containing definitions.
 
 ;;; TODO:
-;; some entries have only references (REFHWD), title is not needed for them
-;; - https://www.ldoceonline.com/dictionary/beyond
+;; * some entries have only references
+;;   these are ignored now, a better way?
+;;   - https://www.ldoceonline.com/dictionary/beyond
+;; * write tests (ERT), find examples
+;;   - a word with no entry but ref
+;;   - a word with no sense etc.
+;;   - good example: 'mind'
+
 
 ;;; Code:
 (require 'url)
 (require 'dom)
 (require 'org)
 (require 'thingatpt)
+(require 'subr-x)
 
 
 (defun longman-lookup--get-node-text (n)
@@ -44,27 +51,39 @@
         (dolist (ex (dom-by-class node "^EXAMPLE$"))
           (setq text (concat text "      - " (longman-lookup--get-node-text ex) "\n"))))
        ((string= (dom-attr node 'class) "Subsense")
-        (setq text (concat text (longman-lookup--parse-sense node))))))
+        (setq text (concat text (longman-lookup--parse-sense node))))
+       ((string= (dom-attr node 'class) "Crossref")
+        (setq text (concat text "  * " (longman-lookup--get-node-text node) "\n")))))
     text))
 
 
-(defun longman-lookup--parse-entry (entry name)
-  "Parse ENTRY with the name NAME."
+(defun longman-lookup--parse-entry (entry)
+  "Parse the entry ENTRY."
   (let* ((pos (string-trim (dom-text (car (dom-by-class entry "^POS$")))))
          (senses (dom-by-class entry "^Sense$"))
-         (text (format "* %s (%s):\n" name pos)))
+         (word (dom-text (car (dom-by-class entry "^HWD$"))))
+         (entry-header (concat "* "
+                               word
+                               (unless (string-empty-p pos) (format " (%s)" pos))
+                               ":\n"))
+         (entry-text ""))
     (dolist (sense senses)
-      (setq text (concat text (longman-lookup--parse-sense sense))))
-    text))
+      (let ((sense-text (longman-lookup--parse-sense sense)))
+        (unless (string-empty-p sense-text)
+          (setq entry-text (concat entry-text sense-text)))))
+    (if (string-empty-p entry-text)
+        nil
+      (concat entry-header entry-text))))
 
 
 ;;;###autoload
 (defun longman-lookup (word)
-  "Fetch the definition of WORD from 'ldoceonline.com' and create an org mode buffer to display it."
+  "Fetch the definition of WORD from 'ldoceonline.com' and display it in an `org-mode' buffer."
   (interactive (list
-                (let ((w (thing-at-point 'word)))
-                  (if w (read-string (format "Enter word (%s): " w)
-                                     nil nil w)
+                (let ((w (if (region-active-p)
+                             (buffer-substring-no-properties (mark) (point))
+                           (thing-at-point 'word))))
+                  (if w (read-string (format "Enter word (%s): " w) nil nil w)
                     (read-string "Enter word: ")))))
   (let* ((entries-text nil)
          (header nil)
@@ -74,27 +93,26 @@
     (unless temp-buf
       (error "Fetch failed: %s\n" u))
     (with-current-buffer temp-buf
-      (let* ((b (point-min))
-             (e (point-max))
-             (tree (libxml-parse-html-region b e))
+      (let* ((tree (libxml-parse-html-region (point-min) (point-max)))
              (entries (dom-by-class tree "^dictentry$")))
         (when (or (null tree)
                   (null entries))
-          (error "Parse failed or necessary information cannot be found."))
+          (error "Parse failed or necessary information cannot be found"))
         (setq header (longman-lookup--get-node-text (car (dom-by-tag tree 'h1))))
-        (setq entries-text (mapconcat
-                            (lambda (e) (longman-lookup--parse-entry e header))
-                            entries
-                            "")))
+        (setq entries-text (mapconcat #'longman-lookup--parse-entry entries "")))
       (kill-buffer))
     (when (or (null header)
               (string-prefix-p "Sorry, there are no results for" header)
               (string-prefix-p "Did you mean" header))
       (error "Word not found: %s\n" word))
-    (let ((buf (get-buffer-create (format "ldoce <%s>" header))))
+    (let ((buf (get-buffer-create (format "*ldoce <%s>*" header))))
       (with-current-buffer buf
+        (when buffer-read-only
+          (setq buffer-read-only nil)
+          (erase-buffer))
         (org-mode)
-        (insert entries-text))
+        (insert entries-text)
+        (setq buffer-read-only t))
       (display-buffer buf))))
 
 
